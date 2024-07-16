c0eaa61ff98e9980d2291d96fb69ef0e483c4ff3
Tirawat <tirawat.saijai@gmail.com>
2024-07-16 21:24:42 +0700

    fix: add app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script lang="ts" setup>
-import { ref } from "vue"
 import { RouterView } from "vue-router"
-const msg = ref<string>("hello world")
 </script>
 <template>
-  {{ msg }}
   <router-view></router-view>
 </template>
