c2a43d44fb401f660ac69c6e2418d0116bcefcf6
Tirawat <tirawat.saijai@gmail.com>
2024-07-24 00:14:47 +0700

    feat: add typing effect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,58 @@
 <script lang="ts" setup>
+import Typewriter from "typewriter-effect/dist/core"
+import { onBeforeUnmount, onMounted, ref } from "vue"
 import { useI18n } from "vue-i18n"
 const { t } = useI18n()
+const fullname_typing = ref()
+
+onMounted(() => {
+  initTypewriter()
+})
+
+onBeforeUnmount(() => {
+  fullname_typing.value.stop()
+})
+
+const initTypewriter = () => {
+  fullname_typing.value = new Typewriter("#typewriter", {
+    loop: true,
+    cursorClassName: "typing-cursor"
+  })
+    .typeString("Front En")
+    .pauseFor(100)
+    .deleteAll()
+    .typeString("Back End D")
+    .pauseFor(100)
+    .deleteAll()
+    .typeString("Fullstca")
+    .pauseFor(120)
+    .deleteAll()
+    .typeString("Full Stack Developer")
+    .callFunction(() => {
+      document.querySelector(".typing-cursor")?.classList.add("typing-cursor-wip")
+    })
+    .pauseFor(1500)
+    .callFunction(() => {
+      document.querySelector(".typing-cursor")?.classList.remove("typing-cursor-wip")
+    })
+    .typeString(`<br>&nbsp&nbsp&nbsp <span class="fs-6 fw-light">but i love front-end 💜</span>`)
+    .callFunction(() => {
+      document.querySelector(".typing-cursor")?.classList.add("typing-cursor-wip")
+    })
+    .pauseFor(6000)
+    .callFunction(() => {
+      document.querySelector(".typing-cursor")?.classList.remove("typing-cursor-wip")
+    })
+    .start()
+}
 </script>
 <template>
   <div class="position-fixed top-50 start-50 translate-middle">
-    <h1>{{ t("personal.fullname") }} ({{ t("personal.nickname") }})</h1>
-    <h3> Fullstack Developer </h3>
+    <div>
+      <h1>{{ t("personal.fullname") }} ({{ t("personal.nickname") }})</h1>
+    </div>
+    <div style="height: 80px">
+      <h3 id="typewriter" class="ps-1"></h3>
+    </div>
   </div>
 </template>
