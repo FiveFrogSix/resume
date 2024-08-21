5e99e241b64284b4d9a70cd90a24f76d18e3286a
Tirawat <tirawat.saijai@gmail.com>
2024-08-21 22:56:01 +0700

    chore: pretty code by win ci/cd

--- win_cicd.bat
+++ win_cicd.bat
@@ -14,22 +14,18 @@ call npm run format
 git add .
 git commit -m "chore: pretty code by win ci/cd"
 git pull
-if errorlevel 1  (
-    pause
-    exit
-)
+if errorlevel 1  ( pause exit ) 
 git push
-if errorlevel 1  (
-    pause
-    exit
-)
+if errorlevel 1  ( pause exit ) 
 
 call npm run lint
 call npm run test:unit
-@REM if errorlevel 1  (
-@REM     pause
-@REM     exit
-@REM ) 
+@REM if errorlevel 1  ( pause exit ) 
+
 call npm run build
+if errorlevel 1  ( pause exit ) 
+call win_cicd.config.bat
+if errorlevel 1  ( pause  exit ) 
+echo %FTP_SERVER%
 pause
 exit
