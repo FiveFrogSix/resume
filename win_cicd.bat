f57c6bbcfdd14e0024b0c6e2d41583d1ea9fce14
Tirawat <tirawat.saijai@gmail.com>
2024-08-22 03:24:46 +0700

    chore: remove some action ci/cd

--- win_cicd.bat
+++ win_cicd.bat
@@ -1,51 +1,53 @@
 @echo off
 cls
 rmdir /s /q dist
-@REM set /p input_branch = branch: 
-@REM git switch %input_branch%
-@REM if errorlevel 1 (
-@REM     pause
-@REM     exit
-@REM )
-git pull 
+
+@REM Check 
 call npm i 
 call npm run format 
 
+@REM Stage: 
+echo Stage: Start
 @REM git add .
 @REM git commit -m "chore: pretty code by win ci/cd"
 @REM git pull
 @REM if errorlevel 1  ( pause exit ) 
 @REM git push
 @REM if errorlevel 1  ( pause exit ) 
+@REM git switch main
+@REM git merge dev
+if errorlevel 1  ( 
+    echo conflict: main merge dev
+    pause > nul
+    exit 
+) 
+@REM git push
 
 call npm run lint 
 call npm run test:unit 
 @REM if errorlevel 1  ( pause exit ) 
 
 call npm run build 
+
 if errorlevel 1  ( 
     pause 
     exit 
 ) 
-if errorlevel 1  ( 
-    pause  
-    exit 
-) 
 
-SET /P AREYOUSURE=Are you sure deploy (Y/N)?: 
-if /I "%AREYOUSURE%" NEQ "Y" ( exit )
 
-@REM require WinSCP
-
+@REM require WinSCP becuase can't use ftp command
 "C:\Program Files (x86)\WinSCP\WinSCP.exe"/script=win_cicd.ftp
 
 if %ERRORLEVEL% neq 0 (
     echo Error occurred during FTP transfer. 
     pause > nul
     exit
-) else (
-    echo FTP transfer completed successfully.
-)
+) 
+
 rmdir /s /q dist
+
+@REM git switch dev
+echo Stage: End
+
 pause
 exit
